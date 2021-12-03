4224de101af3f2816664025e9129445d49440e70
yeonsu0316 <a01064189595@gmail.com>
2021-12-03 18:36:27 +0900

    1203 18:36

--- src/main/java/goo/review_img/model/Review_imgDAO.java
+++ src/main/java/goo/review_img/model/Review_imgDAO.java
@@ -1,6 +1,9 @@
 package goo.review_img.model;
 
+import java.util.*;
+
 public interface Review_imgDAO {
 
-	
+	public int addImgPath(Review_imgDTO dto);
+	public List<Review_imgDTO> review_imgContent(Map map);
 }
